dca7d8237bea29d49925d0909f285f99c462167b
LordSahvith <bulletproofchild@gmail.com>
2024-10-18 15:56:20 -0600

    uncomment code

--- matrix/js/react/online/LinkedIn/Node-Essentials/globals.js
+++ matrix/js/react/online/LinkedIn/Node-Essentials/globals.js
@@ -1,70 +1,70 @@
-// let hello = "Hello World from Node.js";
-// let justNode = hello.slice(17);
-// console.log(`Who let the ${justNode} out?`);
+let hello = "Hello World from Node.js";
+let justNode = hello.slice(17);
+console.log(`Who let the ${justNode} out?`);
 
-// let directory = __dirname;
-// console.log(directory);
-// console.log(__filename);
+let directory = __dirname;
+console.log(directory);
+console.log(__filename);
 
-// const path = require("path");
-// console.log(`The file name is: ${path.basename(__filename)}`);
+const path = require("path");
+console.log(`The file name is: ${path.basename(__filename)}`);
 
-// for (let key in global) {
-//     console.log(key);
-// }
+for (let key in global) {
+    console.log(key);
+}
 
 
 
 // ARGUMENTS
 
-// console.log(process.argv);
+console.log(process.argv);
 
-// function grab(flag) {
-//     let indexAfterFlag = process.argv.indexOf(flag) + 1;
-//     return process.argv[indexAfterFlag];
-// }
+function grab(flag) {
+    let indexAfterFlag = process.argv.indexOf(flag) + 1;
+    return process.argv[indexAfterFlag];
+}
 
-// let greeting = grab("--greeting");
-// let user = grab("--user");
+let greeting = grab("--greeting");
+let user = grab("--user");
 
-// console.log(greeting);
-// console.log(user);
+console.log(greeting);
+console.log(user);
 
 
 
 // STANDARD OUT
 
-// process.stdout.write("Hello \n \n");
+process.stdout.write("Hello \n \n");
 
-// const questions = [
-//     "What is your name?",
-//     "What would you rather be doing?",
-//     "What is your preferred programming language?"
-// ]
+const questions = [
+    "What is your name?",
+    "What would you rather be doing?",
+    "What is your preferred programming language?"
+]
 
-// const answers = [];
+const answers = [];
 
-// function ask(index = 0) {
-//     process.stdout.write(`\n\n\n ${questions[index]}`);
-//     process.stdout.write(` > `);
-// }
+function ask(index = 0) {
+    process.stdout.write(`\n\n\n ${questions[index]}`);
+    process.stdout.write(` > `);
+}
 
-// ask();
+ask();
 
-// process.stdin.on("data", function (data) {
-//     answers.push(data.toString().trim());
+process.stdin.on("data", function (data) {
+    answers.push(data.toString().trim());
 
-//     if (answers.length < questions.length) {
-//         ask(answers.length);
-//     } else {
-//         process.exit();
-//     }
-// });
+    if (answers.length < questions.length) {
+        ask(answers.length);
+    } else {
+        process.exit();
+    }
+});
 
-// process.on("exit", function() {
-//     process.stdout.write("\n\n\n\n");
-//     process.stdout.write(`Go be ${answers[1]} ${answers[0]} you can finish writing ${answers[2]} later.`);
-// });
+process.on("exit", function() {
+    process.stdout.write("\n\n\n\n");
+    process.stdout.write(`Go be ${answers[1]} ${answers[0]} you can finish writing ${answers[2]} later.`);
+});
 
 
 // TIMERS
