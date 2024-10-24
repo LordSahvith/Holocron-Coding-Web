dea1faf183f4d8239dd02384b6d8960fda877744
LordSahvith <bulletproofchild@gmail.com>
2024-10-23 19:54:54 -0600

    useEffect() Hook

--- matrix/js/react/online/LinkedIn/React-Essentials/mine/chapter4/react-app/src/App.js
+++ matrix/js/react/online/LinkedIn/React-Essentials/mine/chapter4/react-app/src/App.js
@@ -1,8 +1,17 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
-function App({ library }) {
+function App() {
   const [emotion, setEmotion] = useState("happy");
+  const [secondary, setSecondary] = useState("tired");
+
+  useEffect(() => {
+    console.log(`It's ${emotion} right now.`);
+  }, [emotion]);
+
+  useEffect(() => {
+    console.log(`It's ${secondary} around here!`);
+  }, [secondary]);
 
   return (
     <div className="App">
@@ -10,6 +19,9 @@ function App({ library }) {
         <h1>Current emotion: {emotion}</h1>
         <button onClick={() => setEmotion("sad")}>Sad</button>
         <button onClick={() => setEmotion("excited")}>Excited</button>
+
+        <h2>Current secondary emotion: {secondary}</h2>
+        <button onClick={() => setSecondary("grateful")}>Grateful</button>
       </header>
     </div>
   );
