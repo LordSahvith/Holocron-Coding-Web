fc5328e3c94253f26dd2339fe28158eabfa8da15
LordSahvith <bulletproofchild@gmail.com>
2024-10-24 16:44:50 -0600

    work with render props; finish ch 6 Async React

--- matrix/js/react/online/LinkedIn/React-Essentials/mine/chapter6/react-app/src/App.js
+++ matrix/js/react/online/LinkedIn/React-Essentials/mine/chapter6/react-app/src/App.js
@@ -1,64 +1,35 @@
 import "./App.css";
-import { useState, useEffect } from "react";
 
-const query = `
-query {
-  allLifts {
-    name
-    elevationGain
-    status
-  }
-}`;
+const tahoePeaks = [
+  { name: "freel", elevation: 10891 },
+  { name: "Monument", elevation: 10067 },
+  { name: "Pyramid", elevation: 9983 },
+  { name: "Tallac", elevation: 9735 },
+];
 
-const options = {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json",
-  },
-  body: JSON.stringify({ query }),
-};
-
-function Lift({ name, evelavtionGain, status }) {
-  return (
-    <div>
-      <h1>{name}</h1>
-      <p>
-        {evelavtionGain} {status}
-      </p>
-    </div>
+function List({ data, renderItem, renderEmtpy }) {
+  return !data.length ? (
+    renderEmtpy
+  ) : (
+    <ul>
+      {data.map((item) => (
+        <li key={item.name}>{renderItem(item)}</li>
+      ))}
+    </ul>
   );
 }
 
 function App() {
-  const [data, setData] = useState(null);
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    setLoading(true);
-    fetch("https://snowtooth.moonhighway.com/", options)
-      .then((response) => response.json())
-      .then((data) => setData(data.data))
-      .then(() => setLoading(false))
-      .catch(setError);
-  }, []);
-
-  if (loading) return <h1>Loading....</h1>;
-  if (error) return <pre>{JSON.stringify(error)}</pre>;
-  if (!data) return null;
-
-  console.log(data);
-
   return (
-    <div>
-      {data.allLifts.map((lift) => (
-        <Lift
-          name={lift.name}
-          evelavtionGain={lift.evelavtionGain}
-          status={lift.status}
-        />
-      ))}
-    </div>
+    <List
+      data={tahoePeaks}
+      renderEmtpy={<p>This list is empty</p>}
+      renderItem={(item) => (
+        <>
+          {item.name} - {item.elevation} ft.
+        </>
+      )}
+    />
   );
 }
 
