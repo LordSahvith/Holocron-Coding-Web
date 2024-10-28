8b77b1fcca3fe047dcb8bda5bab4e69346d9aa5b
LordSahvith <bulletproofchild@gmail.com>
2024-10-28 12:56:15 -0600

    video out of date
    Don't want to get side tracked to fix the instructors error by not updating with time

--- matrix/js/react/online/LinkedIn/React-Essentials/mine/chapter7/react-app/src/App.js
+++ matrix/js/react/online/LinkedIn/React-Essentials/mine/chapter7/react-app/src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
-function App() {
+function Home() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Home</h1>
     </div>
   );
+}
+
+function About() {
+  return (
+    <div>
+      <h1>About</h1>
+    </div>
+  );
+}
+
+function Contact() {
+  return (
+    <div>
+      <h1>Contact</h1>
+    </div>
+  );
+}
+
+function App() {
+  return <Home />;
 }
 
 export default App;
