aeadbd029f76e1f6f43f6f6da79ee7f4fcd9ed91
LordSahvith <bulletproofchild@gmail.com>
2024-11-04 13:31:34 -0700

    course is more focused on styles and doesn't explain shit very well

--- matrix/js/react/online/LinkedIn/Build-Interface/mine/react-interface-app/src/App.js
+++ matrix/js/react/online/LinkedIn/Build-Interface/mine/react-interface-app/src/App.js
@@ -1,10 +1,12 @@
 import { BiArchive } from "react-icons/bi";
+import Search from "./Components/Search";
 
 function App() {
   return (
     <div className="App container mx-auto mt-3 font-thin">
       <h1 className="text-5xl">Your Appointments</h1>
       <BiArchive />
+      <Search />
     </div>
   );
 }
