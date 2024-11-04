01a5bc8787e119965b3feb18c75ee88ec1ee71cc
LordSahvith <bulletproofchild@gmail.com>
2024-11-04 13:23:33 -0700

    why? we here for react, not styling

--- matrix/js/react/online/LinkedIn/Build-Interface/mine/react-interface-app/src/App.js
+++ matrix/js/react/online/LinkedIn/Build-Interface/mine/react-interface-app/src/App.js
@@ -1,8 +1,10 @@
+import { BiArchive } from "react-icons/bi";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Your Appointments</h1>
+    <div className="App container mx-auto mt-3 font-thin">
+      <h1 className="text-5xl">Your Appointments</h1>
+      <BiArchive />
     </div>
   );
 }
