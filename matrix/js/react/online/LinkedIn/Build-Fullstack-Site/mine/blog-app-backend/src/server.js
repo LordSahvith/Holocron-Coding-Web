bf595d2e3965287a5419c6c38d096a79b52d5bf6
LordSahvith <bulletproofchild@gmail.com>
2024-11-08 14:34:11 -0700

    setting up postman

--- matrix/js/react/online/LinkedIn/Build-Fullstack-Site/mine/blog-app-backend/src/server.js
+++ matrix/js/react/online/LinkedIn/Build-Fullstack-Site/mine/blog-app-backend/src/server.js
@@ -1,9 +1,15 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
 
 app.get("/hello", (req, res) => {
   res.send("hello");
+});
+
+app.post("/post", (req, res) => {
+  console.log(req.body);
+  res.send(`Hello ${req.body.name}`);
 });
 
 app.listen(8000, () => {
