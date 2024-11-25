414f26149f3281581a8f8f7c82a9d9aaaf8c8fe4
LordSahvith <bulletproofchild@gmail.com>
2024-11-25 15:58:44 -0700

    add comments

--- matrix/js/react/online/LinkedIn/Build-Fullstack-Site/mine/blog-app-backend/src/server.js
+++ matrix/js/react/online/LinkedIn/Build-Fullstack-Site/mine/blog-app-backend/src/server.js
@@ -4,14 +4,17 @@ let articlesInfo = [
   {
     name: "learn-react",
     upvotes: 0,
+    comments: [],
   },
   {
     name: "learn-node",
     upvotes: 0,
+    comments: [],
   },
   {
     name: "learn-mongodb",
     upvotes: 0,
+    comments: [],
   },
 ];
 
@@ -24,9 +27,27 @@ app.put("/api/articles/:name/upvote", (req, res) => {
 
   if (article) {
     article.upvotes++;
-    res.send(`The ${name} article now has ${article.upvotes} upvote${article.upvotes > 1 ? 's' : ''}!`);
+    res.send(
+      `The ${name} article now has ${article.upvotes} upvote${
+        article.upvotes > 1 ? "s" : ""
+      }!`
+    );
   } else {
     res.send("That article doesn't exist");
+  }
+});
+
+app.post("/api/articles/:name/comments", (req, res) => {
+  const { name } = req.params;
+  const { postedBy, text } = req.body;
+
+  const article = articlesInfo.find((article) => article.name === name);
+
+  if (article) {
+    article.comments.push({ postedBy, text });
+    res.send(article.comments);
+  } else {
+    res.send("That article doesn't exist.");
   }
 });
 
