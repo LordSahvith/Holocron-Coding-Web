3ea87bed1ae6defb8fdc12994977c637bc1b2618
Lord Savith <bulletproofchild@gmail.com>
2025-06-06 16:26:27 -0600

    add comments to functions-as-values.js

--- matrix/js/js/books/eloquent_js/03-functions/01-functions-as-values.js
+++ matrix/js/js/books/eloquent_js/03-functions/01-functions-as-values.js
@@ -1,14 +1,14 @@
 let launchMissile = function (x, y) {
-  console.log("Launching missile at " + x + ", " + y + "!");
+  console.log('Launching missile at ' + x + ', ' + y + '!');
 };
 
-launchMissile("1", "2");
+launchMissile('1', '2'); // Launching missile at 1, 2!
 
 // The function can be redefined later
 if (true) {
-    launchMissile = function () {
-        console.log("Missile launch aborted!");
-    };
+  launchMissile = function () {
+    console.log('Missile launch aborted!');
+  };
 }
 
-launchMissile();
+launchMissile(); // Missile launch aborted!
