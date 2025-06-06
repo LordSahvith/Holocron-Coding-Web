90d5044a334435a6d629d4490b62b09a5624219a
Lord Savith <bulletproofchild@gmail.com>
2025-06-06 16:26:57 -0600

    add comments to closure.js

--- matrix/js/js/books/eloquent_js/03-functions/04-closure.js
+++ matrix/js/js/books/eloquent_js/03-functions/04-closure.js
@@ -6,7 +6,7 @@
  */
 
 /**
- * This is a closure because it captures the `local` variable (inner scope) 
+ * This is a closure because it captures the `local` variable (inner scope)
  * which references the `n` variable (outer scope) and the
  * returned function has access to the `local` variable
  * @param {*} n
@@ -15,14 +15,14 @@
  */
 function wrapValue(n) {
   let local = n;
-  return () => local;
+  return () => local; // closure: captures the `local` variable in a returned function
 }
 
 /**
  * The inner function (() => local) has access to the
- * outer function's scope (wrapValue(n))
- * even after the outer function has returned becuase
- * the outer function's scope is preserved in the closure
+ * outer function's scope (wrapValue(n)) even after 
+ * the outer function has returned because the 
+ * outer function's scope is preserved in the closure
  */
 const wrap1 = wrapValue(1); // value: () => local; local === 1
 const wrap2 = wrapValue(2); // value: () => local; local === 2
@@ -38,9 +38,17 @@ console.log(multiply(2, 5)); // 10
 console.log(multiply(3, 5)); // 15
 console.log(multiply(4, 5)); // 15
 
-// version 2 - closure
+/**
+ * Version 2 - Closure:
+ * Version 2 is a closure because it captures the `factor` variable
+ * in the returned function, allowing it to be used later
+ * @param {number} factor - the factor to multiply by
+ * @returns number => number * factor - a function that takes a number
+ * and returns the product of the number and the factor
+ * @example multiplier(2)(5) === 10
+ */
 function multiplier(factor) {
-  return number => number * factor; // captures the `factor` variable in a returned function
+  return number => number * factor; // closure: captures the `factor` variable in a returned function
 }
 
 let double = multiplier(2); // value: number => number * factor; factor === 2
@@ -48,7 +56,15 @@ let triple = multiplier(3); // value: number => number * factor; factor === 3
 console.log(double(5)); // double = number(5) => number(5) * factor(2); 10
 console.log(triple(5)); // triple = number(5) => number(5) * factor(3); 15
 
-// version 3 - combined (function declaration + closure)
+/**
+ * Version 3 - Combined (Function Declaration + Closure):
+ * This version combines the function declaration with a closure
+ * to create a multiplier function that captures the factor
+ * @param {*} factor - the factor to multiply by
+ * @returns {function} - a function (with a closure 'factor') that takes
+ * a number and returns the product of the number and the factor
+ * @example multiplier2(4)(5) === 20
+ */
 function multiplier2(factor) {
   return function (number) {
     return multiply(number, factor); // closure: captures `factor` variable
