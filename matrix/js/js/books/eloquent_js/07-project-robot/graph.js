c0d483a90ce07876d0fbaa264622bac5d955a874
LordSahvith <bulletproofchild@gmail.com>
2025-06-10 01:53:57 -0600

    Chapter 7: meadowfield & the task

---
+++ matrix/js/js/books/eloquent_js/07-project-robot/graph.js
@@ -0,0 +1,21 @@
+const { roads } = require('./data/roads');
+
+function buildGraph(edges) {
+  let graph = Object.create(null);
+  function addEdge(from, to) {
+    if (from in graph) {
+      graph[from].push(to);
+    } else {
+      graph[from] = [to];
+    }
+  }
+
+  for (let [from, to] of edges.map(road => road.split('-'))) {
+    addEdge(from, to);
+    addEdge(to, from);
+  }
+
+  return graph;
+}
+
+module.exports = buildGraph(roads);
---
+++ matrix/js/js/books/eloquent_js/07-project-robot/robot.js
@@ -0,0 +1,8 @@
+const VillageState = require('./village');
+
+let first = new VillageState('Post Office', [{ place: 'Post Office', address: "Alice's House" }]);
+let next = first.move("Alice's House");
+
+console.log(next.place); // Alice's House
+console.log(next.parcels); // []
+console.log(first.place); // Post Office
