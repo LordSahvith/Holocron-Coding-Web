80acb3a0d3976e44c1c36a7777a6ae7095915393
Lord Savith <bulletproofchild@gmail.com>
2025-06-10 19:00:12 -0600

    Chapter 8: debugging

--- matrix/js/js/books/eloquent_js/07-project-robot/lib/helpers.js
+++ matrix/js/js/books/eloquent_js/07-project-robot/lib/helpers.js
@@ -1,4 +1,8 @@
-// random number between 0 - array.length for a random index of array
+/**
+ * picks a random number between 0 - array.length for a random index of array
+ * @param {array} array - array that needs a random index for a random choice
+ * @returns {*} the value from the random index
+ */
 function randomPick(array) {
   let choice = Math.floor(Math.random() * array.length);
   return array[choice];
