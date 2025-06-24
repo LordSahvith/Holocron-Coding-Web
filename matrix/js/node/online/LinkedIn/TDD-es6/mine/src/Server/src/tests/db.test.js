8251119dd9edb5d508539ed2ffba9b40ba6eae57
Lord Savith <bulletproofchild@gmail.com>
2025-06-24 15:58:22 -0600

    Test: start db.js

--- matrix/js/node/online/LinkedIn/TDD-es6/mine/src/Server/src/tests/db.test.js
+++ matrix/js/node/online/LinkedIn/TDD-es6/mine/src/Server/src/tests/db.test.js
@@ -1,9 +1,9 @@
 import { MongoClient } from 'mongodb';
 import { expect } from 'chai';
-import { getUserByUsername } from '..db';
+import { getUserByUsername } from '../db';
 
 describe('getUserByUsername', () => {
-  it('get the correct user from the database given a username', () => {
+  it('get the correct user from the database given a username', async () => {
     const client = await MongoClient.connect(
       `mongodb://localhost:27017/TEST_DB`,
       {
@@ -14,8 +14,34 @@ describe('getUserByUsername', () => {
 
     const db = client.db('TEST_DB');
 
-    // tests
+    const fakeData = [
+      {
+        id: '123',
+        username: 'abc',
+        email: 'abc@gmail.com',
+      },
+      {
+        id: '124',
+        username: 'wrong',
+        email: 'wrong@wrong.com',
+      },
+    ];
 
+    await db.collection('users').insertMany(fakeData);
+    const actual = await getUserByUsername('abc');
+    const finalDBState = await db.collection('users').find().toArray();
+
+    // drop and close before assertions in case of failure
+    await db.dropDatabase();
     client.close();
+
+    const expected = {
+      id: '123',
+      username: 'abc',
+      email: 'abc@gmail.com',
+    };
+
+    expect(actual).to.deep.equal(expected);
+    expect(finalDBState).to.deep.equal(fakeData);
   });
 });
