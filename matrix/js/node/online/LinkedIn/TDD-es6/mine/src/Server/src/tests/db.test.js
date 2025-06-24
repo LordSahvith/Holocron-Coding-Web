8e25bdc31ca2c6359a8b26ac4eb4bf1b6e6c6ebe
Lord Savith <bulletproofchild@gmail.com>
2025-06-24 16:23:21 -0600

    use mocha's afterEach() function to reset database after each test

--- matrix/js/node/online/LinkedIn/TDD-es6/mine/src/Server/src/tests/db.test.js
+++ matrix/js/node/online/LinkedIn/TDD-es6/mine/src/Server/src/tests/db.test.js
@@ -7,6 +7,10 @@ import {
 } from '../../helpers/test-helpers';
 
 describe('getUserByUsername', () => {
+  afterEach('reset the database', async () => {
+    await resetDatabase();
+  });
+
   it('get the correct user from the database given a username', async () => {
     const fakeData = [
       {
@@ -25,8 +29,6 @@ describe('getUserByUsername', () => {
 
     const actual = await getUserByUsername('abc');
     const finalDBState = await getDatabaseData('users');
-
-    await resetDatabase();
 
     const expected = {
       id: '123',
