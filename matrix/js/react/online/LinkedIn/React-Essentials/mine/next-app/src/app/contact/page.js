6425bed61c5eccb0438d69bb2a4c88b800356432
Lord Savith <bulletproofchild@gmail.com>
2025-06-25 15:18:52 -0600

    create a form

--- matrix/js/react/online/LinkedIn/React-Essentials/mine/next-app/src/app/contact/page.js
+++ matrix/js/react/online/LinkedIn/React-Essentials/mine/next-app/src/app/contact/page.js
@@ -2,7 +2,36 @@ export default function Page() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <h1>Contact</h1>
+        <h1 className="text-2xl font-bold text-center mb-6 w-100">Contact</h1>
+        <form className="max-w-md mx-auto p-6 bg-white shadow-md rounded-md text-black space-y-4">
+          <div>
+            <label htmlFor="email" className="block text-sm font-medium">
+              Email:{' '}
+            </label>
+            <input
+              id="email"
+              type="email"
+              name="email"
+              required
+              className="border border-gray"
+            />
+          </div>
+          <div>
+            <label htmlFor="message" className="block text-sm font-medium">
+              Message:{' '}
+            </label>
+            <textarea
+              id="message"
+              name="message"
+              rows={4}
+              required
+              className="border border-gray"
+            />
+          </div>
+          <button type="submit" className="border border-gray p-2">
+            Send Message
+          </button>
+        </form>
       </main>
     </div>
   );
