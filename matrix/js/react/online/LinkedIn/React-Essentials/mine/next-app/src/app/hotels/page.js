4b802f26df897837ef4e20ff6a9f11ec4e638876
Lord Savith <bulletproofchild@gmail.com>
2025-06-25 14:50:32 -0600

    passing data as props

--- matrix/js/react/online/LinkedIn/React-Essentials/mine/next-app/src/app/hotels/page.js
+++ matrix/js/react/online/LinkedIn/React-Essentials/mine/next-app/src/app/hotels/page.js
@@ -1,8 +1,33 @@
-export default function Page() {
+async function getData() {
+  const res = await fetch('https://snowtooth-hotel-api.fly.dev');
+  return res.json();
+}
+
+function HotelBlock({ name, capacity }) {
+  return (
+    <div className="p-4">
+      <h2>{name}</h2>
+      <p>Capacity: {capacity}</p>
+    </div>
+  );
+}
+
+export default async function Page() {
+  const data = await getData();
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <h1>Hotel Details</h1>
+        {console.log(data)}
+        <section>
+          {data.map(hotel => (
+            <HotelBlock
+              key={hotel.id}
+              name={hotel.name}
+              capacity={hotel.capacity}
+            />
+          ))}
+        </section>
       </main>
     </div>
   );
