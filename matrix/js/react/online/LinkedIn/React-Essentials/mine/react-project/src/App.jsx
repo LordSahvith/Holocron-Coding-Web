7471a7ffd2d6e41c8c69e02c16ff607eab7a7c35
Lord Savith <bulletproofchild@gmail.com>
2025-06-25 12:55:05 -0600

    creating react elements

--- matrix/js/react/online/LinkedIn/React-Essentials/mine/react-project/src/App.jsx
+++ matrix/js/react/online/LinkedIn/React-Essentials/mine/react-project/src/App.jsx
@@ -1,7 +1,16 @@
 import './App.css';
 
+function Header({ title }) {
+  return (
+    <header>
+      <h1>{title}</h1>
+      <h2>We serve the most delicious food around.</h2>
+    </header>
+  );
+}
+
 function App() {
-  return <h1>Hello React Projcet</h1>;
+  return <Header title="Lord Savith's Kitchen" />;
 }
 
 export default App;
