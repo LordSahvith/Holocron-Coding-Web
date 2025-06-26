55f0c803006bb4c42733f28fcf26654a9df981ca
Lord Savith <bulletproofchild@gmail.com>
2025-06-25 18:15:11 -0600

    Route Parameters in Express

--- matrix/js/full-stack/MERN/online/LinkedIn/Create-Host-Fullstack-Site/mine/back-end/src/server.js
+++ matrix/js/full-stack/MERN/online/LinkedIn/Create-Host-Fullstack-Site/mine/back-end/src/server.js
@@ -8,6 +8,10 @@ app.get('/hello', (req, res) => {
   res.send('Hello from a GET endpooint');
 });
 
+app.get('/hello/:name', (req, res) => {
+  res.send(`Hello, ${req.params.name} from a GET endpooint`);
+});
+
 app.post('/hello', (req, res) => {
   res.send(`Hello, ${req.body.name}, from a POST endpoint`);
 });
