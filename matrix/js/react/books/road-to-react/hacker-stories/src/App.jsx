7c017db84ac366d34c06745ca79c772e53ce1669
LordSahvith <bulletproofchild@gmail.com>
2025-06-30 01:15:26 -0600

    React Lists

--- matrix/js/react/books/road-to-react/hacker-stories/src/App.jsx
+++ matrix/js/react/books/road-to-react/hacker-stories/src/App.jsx
@@ -9,13 +9,52 @@ function getGreeting({ greeting, title }) {
   return `${greeting} ${title}`;
 }
 
+const numbers = [1, 2, 3, 4];
+
+const exponentialNumbers = numbers.map(number => {
+  return number * number;
+});
+
+console.log(exponentialNumbers);
+
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
 function App() {
   return (
     <section>
       <h1>{getGreeting(welcome)}</h1>
 
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
+      <ul>
+        {list.map(item => {
+          return (
+            <li key={item.objectID}>
+              <span>
+                <a href={item.url}>{item.title}</a>
+              </span>
+              <span>{item.author}</span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
+            </li>
+          );
+        })}
+      </ul>
     </section>
   );
 }
