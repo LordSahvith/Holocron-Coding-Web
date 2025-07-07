ec497ca8256623ada1fea3ea431796dbf46a2590
LordSahvith <bulletproofchild@gmail.com>
2025-07-07 00:11:58 -0600

    React: Data Re-Fetching

--- matrix/js/react/books/road-to-react/hacker-stories/src/App.jsx
+++ matrix/js/react/books/road-to-react/hacker-stories/src/App.jsx
@@ -7,7 +7,7 @@ import './App.css';
 const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
 
 function App() {
-  const [searchTerm, setSearchTerm] = useStorageState('search', '');
+  const [searchTerm, setSearchTerm] = useStorageState('search', 'react');
 
   const [stories, dispatchStories] = useReducer(storiesReducer, {
     data: [],
@@ -16,9 +16,11 @@ function App() {
   });
 
   useEffect(() => {
+    if (!searchTerm) return;
+
     dispatchStories({ type: 'STORIES_FETCH_INIT' });
 
-    fetch(`${API_ENDPOINT}react`)
+    fetch(`${API_ENDPOINT}${searchTerm}`)
       .then(response => response.json())
       .then(result => {
         dispatchStories({
@@ -27,7 +29,7 @@ function App() {
         });
       })
       .catch(() => dispatchStories({ type: 'STORIES_FETCH_FAILURE' }));
-  }, []);
+  }, [searchTerm]);
 
   const handleRemoveStory = item => {
     dispatchStories({
@@ -39,10 +41,6 @@ function App() {
   const handleSearch = function (event) {
     setSearchTerm(event.target.value);
   };
-
-  const searchedStories = stories.data.filter(story =>
-    story.title.toLowerCase().includes(searchTerm.toLowerCase())
-  );
 
   return (
     <section>
@@ -65,7 +63,7 @@ function App() {
       {stories.isLoading ? (
         <p>Loading...</p>
       ) : (
-        <List list={searchedStories} onRemoveItem={handleRemoveStory} />
+        <List list={stories.data} onRemoveItem={handleRemoveStory} />
       )}
     </section>
   );
