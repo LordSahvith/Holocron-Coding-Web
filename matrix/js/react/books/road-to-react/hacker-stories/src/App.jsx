087a5182095aec1c0a7576b84de55a9971f4b819
LordSahvith <bulletproofchild@gmail.com>
2025-07-06 21:46:51 -0600

    React: Conditional Rendering

--- matrix/js/react/books/road-to-react/hacker-stories/src/App.jsx
+++ matrix/js/react/books/road-to-react/hacker-stories/src/App.jsx
@@ -7,8 +7,9 @@ import './App.css';
 
 function App() {
   const [searchTerm, setSearchTerm] = useStorageState('search', '');
-
   const [stories, setStories] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
   function getAsyncStories() {
     return new Promise(resolve =>
@@ -17,9 +18,14 @@ function App() {
   }
 
   useEffect(() => {
-    getAsyncStories().then(result => {
-      setStories(result.data.stories);
-    });
+    setIsLoading(true);
+
+    getAsyncStories()
+      .then(result => {
+        setStories(result.data.stories);
+        setIsLoading(false);
+      })
+      .catch(() => setIsError(true));
   }, []);
 
   const handleRemoveStory = item => {
@@ -54,7 +60,13 @@ function App() {
 
       <hr />
 
-      <List list={searchedStories} onRemoveItem={handleRemoveStory} />
+      {isError && <p>Something went wrong...</p>}
+
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <List list={searchedStories} onRemoveItem={handleRemoveStory} />
+      )}
     </section>
   );
 }
