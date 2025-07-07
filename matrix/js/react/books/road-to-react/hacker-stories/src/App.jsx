6730a83dfcb2244bce4478839fd00e45cb428c39
LordSahvith <bulletproofchild@gmail.com>
2025-07-07 01:48:24 -0600

    React: 3rd Party Libraries

--- matrix/js/react/books/road-to-react/hacker-stories/src/App.jsx
+++ matrix/js/react/books/road-to-react/hacker-stories/src/App.jsx
@@ -2,6 +2,7 @@ import { useCallback, useEffect, useReducer, useState } from 'react';
 import InputWithLabel from './components/InputWithLabel';
 import List from './components/List';
 import { useStorageState, storiesReducer } from './lib/helper';
+import axios from 'axios';
 import './App.css';
 
 const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
@@ -20,12 +21,12 @@ function App() {
 
     dispatchStories({ type: 'STORIES_FETCH_INIT' });
 
-    fetch(url)
-      .then(response => response.json())
+    axios
+      .get(url)
       .then(result => {
         dispatchStories({
           type: 'STORIES_FETCH_SUCCESS',
-          payload: result.hits,
+          payload: result.data.hits,
         });
       })
       .catch(() => dispatchStories({ type: 'STORIES_FETCH_FAILURE' }));
