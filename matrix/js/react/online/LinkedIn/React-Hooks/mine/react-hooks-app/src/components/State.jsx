9612e45a9081cdc208c0d4c7354a4b05f04114b3
Lord Savith <bulletproofchild@gmail.com>
2025-07-17 03:12:06 -0600

    Update: Move State into component

---
+++ matrix/js/react/online/LinkedIn/React-Hooks/mine/react-hooks-app/src/components/State.jsx
@@ -0,0 +1,57 @@
+import { useState } from 'react';
+import { FaStar } from 'react-icons/fa';
+
+const createArray = length => [...Array(length)];
+
+function Star({ selected = false, onSelect }) {
+  return <FaStar color={selected ? 'red' : 'gray'} onClick={onSelect} />;
+}
+
+function StarRating({ totalStars = 5 }) {
+  const [selectedStars, setSelectedStars] = useState(0);
+  return (
+    <>
+      {createArray(totalStars).map((n, i) => (
+        <Star
+          key={i}
+          selected={selectedStars > i}
+          onSelect={() => setSelectedStars(i + 1)}
+        />
+      ))}
+      <p>
+        {selectedStars} of {totalStars}
+      </p>
+    </>
+  );
+}
+
+function State() {
+  const [status, setStatus] = useState('Not Delivered');
+  const [checked, setChecked] = useState(false);
+  return (
+    <div>
+      <h2>useState()</h2>
+      <div>
+        <p>Package Status: {status}</p>
+        <button onClick={() => setStatus('Devlivered')}>Deliver</button>
+      </div>
+
+      <div>
+        <label>
+          {checked ? 'checked' : 'not checked'}
+          <input
+            type="checkbox"
+            value={checked}
+            onChange={() => setChecked(checked => !checked)}
+          />
+        </label>
+      </div>
+
+      <div>
+        <StarRating totalStars={5} />
+      </div>
+    </div>
+  );
+}
+
+export default State;
