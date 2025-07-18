4cf825e866ed69d84874e56408a26e3216eb13ae
Lord Savith <bulletproofchild@gmail.com>
2025-07-18 15:48:34 -0600

    Add: Styled Components

--- matrix/js/react/online/LinkedIn/Modern-Projects/todo-list-app/src/components/TodoListItem.jsx
+++ matrix/js/react/online/LinkedIn/Modern-Projects/todo-list-app/src/components/TodoListItem.jsx
@@ -1,12 +1,20 @@
 import { useDispatch } from 'react-redux';
 import { markTodoAsComplete } from './todoSlice';
 import { deleteTodo } from './thunks';
+import styled from 'styled-components';
+
+const CardContainer = styled.div`
+  border-radius: 10px;
+  ${props => !props.$completed && 'background: gray;'}
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.6);
+  padding: 16px;
+`;
 
 function TodoListItem({ todo }) {
   const dispatch = useDispatch();
 
   return (
-    <div>
+    <CardContainer $completed={todo.isCompleted}>
       <h3>{todo.text}</h3>
       {todo.isCompleted && <p>Complete!</p>}
       {todo.isCompleted ? (
@@ -18,7 +26,7 @@ function TodoListItem({ todo }) {
           Mark as Completed
         </button>
       )}
-    </div>
+    </CardContainer>
   );
 }
 
